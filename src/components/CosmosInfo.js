d2bff987a8e07cec3ff3d7246e37e622120712b3
Schrese <53787633+Schrese@users.noreply.github.com>
2019-10-10 18:42:39 -0400

    still can't make the date work

--- src/components/CosmosInfo.js
+++ src/components/CosmosInfo.js
@@ -1,17 +1,41 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import CosmosStructure from './CosmosStructure.js';
+import { Container, Row, Col } from 'reactstrap';
+import {Jumbotron} from 'reactstrap';
+import styled from 'styled-components';
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
 
+const Description = styled.h4`
+    color: blue;
+    text-align: center;
+    width: 80vw;
+    height: 20vh;
+    padding-left: 10%
+`
+const Wrapper = styled.div`
+    height: 100vh;
+    background: lightblue;
+    margin: 0 auto;
+`
+const ImageContainer = styled.img`
+    height: 60vh;
+    
+`
 
 const CosmosInfo = () => {
     const [picture, setPicture] = useState({});
-    // const [newDate, setNewDate]
+    // const [startDate, setStartDate] = useState(new Date());
+    //     console.log(startDate);
+    const [newDate, setNewDate] = useState({});
 
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=JwcanU4s2iCXXkqt3sxgIzNVX1FpNLrlsdEAc92h`)
         .then(response => {
-            console.log(response.data);
+            // console.log(response.data);
             setPicture(response.data)
+            
         })
         .catch(error => {
             console.log('what did you do now?', error);
@@ -20,20 +44,26 @@ const CosmosInfo = () => {
 
     return (
         // <div>Hello</div>
-        <div className = 'cosmos'>
-            {/* <img src = 'response.data.url' alt = 'starburst'/> */}
-            {/* <button onClick = {() => setNewDate('date' -1)}>-</button> */}
-            <h1>The Image of the Day is: {picture.title}</h1>
-            <img className = 'image' src = {picture.url} alt = 'photo of the day'/>
-            <h2>{picture.date}</h2>
-            <h4>{picture.explanation}</h4>
-
-            {/* <CosmosStructure key={picture.url}/> */}
-        
-        </div>
+        <Jumbotron>
+            
+            <Wrapper>
+            
+                <h1>The Image of the Day is: {picture.title}</h1>
+                <ImageContainer className = 'image' src = {picture.url} alt = 'photo of the day'/>
+                <h2>{picture.date}</h2>
+                {/* <DatePicker selected={startDate} onChange={date => setStartDate(date)} dateFormat = 'yyyy-MM-dd' /> */}
+                
+                <form>
+                    <input type='date' 
+                    onChange={newDate => setNewDate()} 
+                    />
+                </form>
+                <Description>{picture.explanation}</Description>
+                {/* <CosmosStructure key={picture.url}/> */}
+                </Wrapper>
+        </Jumbotron>
     )
 }
-
 //my api key: JwcanU4s2iCXXkqt3sxgIzNVX1FpNLrlsdEAc92h 
 
 export default CosmosInfo;
--- src/components/CosmosStructure.js
+++ src/components/CosmosStructure.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-const CosmosStructure = props => {
-    return(
-    <div className = 'img-container' key = {props.url}>
-        <img>{props.url}</img>
-        <p>{props.date}</p>
-    </div>
-  )
-}
+
 
 export default CosmosStructure
